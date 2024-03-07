91c7c5bbe0fb84d18f78f4fe72ebfcd44e556aef
Kethelyn Isabelle <126088212+kethelynisabe@users.noreply.github.com>
2024-03-06 21:08:41 -0300

    Add files via upload

---
+++ introdução js/1_tipos_primitivos,/1_number/scrip.js
@@ -0,0 +1,10 @@
+const texto1 = "kaike cupin";
+const texto2 = "gabizin";
+
+const texto# = "O gabizin foi no banheiro";
+
+console.log(texto1);
+console.log(texto2);
+console.log(texto3);
+
+console.log(texto2 + texto3);
---
+++ introdução js/1_tipos_primitivos,/2_string/scrip.js
@@ -0,0 +1,17 @@
+//tipo number
+
+const meuNumero = 11;
+
+const telefone = 44984599619;
+
+const idade = 17;
+
+
+console.log(meuNumero);
+console.log(telefone);
+console.log(idade); 
+
+
+const some = meuNumero + idade;
+
+console.long(some);
