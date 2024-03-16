32337a18f162bed5932a4b22e5dbd72800a0bbaa
Kethelyn Isabelle <126088212+kethelynisabe@users.noreply.github.com>
2024-03-15 22:13:19 -0300

    Add files via upload

---
+++ introJS3-24-main/introdução js/1_tipos_primitivos,/2_string/scrip.js
@@ -0,0 +1,18 @@
+const text1 = "Olá mundo";
+const text2 = "Olá mundo";
+const senha = "supersegura"
+
+//concatenacao(+)
+
+const citacao = "Meu nome é";
+const meunome = "kethelyn"
+console.log(citacao + meunome);
+
+//compracao
+
+console.log(text1 === text2);
+
+const n1 = 1;
+const n2 = 2;
+
+console.log(n1 === n2);
