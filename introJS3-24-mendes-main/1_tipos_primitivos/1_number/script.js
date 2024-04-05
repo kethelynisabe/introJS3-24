7050dd4f4b2afd283dc33090f8a2122de3514a32
Kethelyn Isabelle <126088212+kethelynisabe@users.noreply.github.com>
2024-04-05 19:51:37 -0300

    Add files via upload

---
+++ introJS3-24-mendes-main/1_tipos_primitivos/1_number/script.js
@@ -0,0 +1,17 @@
+//tipo number
+
+const meuNumero = 11;
+
+const telefone = 4484599619;
+
+const idade = 17;
+
+
+console.log(meuNumero);
+console.log(telefone);
+console.log(idade);
+
+
+const soma = meuNumero + idade;
+
+console.log(soma);
